49aefd4dc41c4f75703ebb5990ca84f4b75a73cf
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-22 06:33:24 -0400

    Add files via upload

---
+++ b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float f, C;
+//enter temperature in celsius
+cout<<"enter the temperature in celusius" << "\n";
+//store the value in Celusius
+cin >> C ;
+//convert the celsius into fahrenheit
+f=(C*9/5 +32);
+//print the terminal
+cout <<"enter the temperature in fahrenheit" << f <<"\n";
+return 0;
+}
